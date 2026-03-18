d69fbc39090affaad8bc3934a20adc5f6f0ebdd7
Abby <abby@tunafish.com>
2026-03-18 21:15:21 +0000

    Failed test

--- index.test.js
+++ index.test.js
@@ -1,33 +1,29 @@
 const add = require('./index');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+test('adds 1 + 2 to equal 4', () => {
+  expect(add(1, 2)).toBe(4);
 });
+
+
+
 
 // sudo docker run -d \
 //   --name=drone \
+//   --network=drone \
 //   -p 8080:80 \
 //   -e DRONE_GITHUB_CLIENT_ID=Ov23liN4YUQmj4WvrZMk \
 //   -e DRONE_GITHUB_CLIENT_SECRET=1a6f41c98997376ab57303506e16be620d68924a \
 //   -e DRONE_RPC_SECRET=supersecret \
-//   -e DRONE_SERVER_HOST=localhost:8080 \
+//   -e DRONE_SERVER_HOST=drone:8080 \
 //   -e DRONE_SERVER_PROTO=http \
 //   drone/drone:latest
 
-// docker run -d \
-//   --name=drone \
-//   -p 8080:80 \
-//   -e DRONE_GITHUB_CLIENT_ID=YOUR_ID \
-//   -e DRONE_GITHUB_CLIENT_SECRET=YOUR_SECRET \
-//   -e DRONE_RPC_SECRET=supersecret \
-//   -e DRONE_SERVER_HOST=host.docker.internal:8080 \
-//   -e DRONE_SERVER_PROTO=http \
-//   drone/drone:latest
-
-// docker run -d \
+// sudo docker run -d \
 //   --name=drone-runner \
-//   -p 3000:3000 \
+//   --network=drone \
 //   -e DRONE_RPC_PROTO=http \
-//   -e DRONE_RPC_HOST=host.docker.internal:8080 \
+//   -e DRONE_RPC_HOST=drone \
 //   -e DRONE_RPC_SECRET=supersecret \
+//   -e DRONE_RUNNER_CAPACITY=2 \
+//   -v /var/run/docker.sock:/var/run/docker.sock \
 //   drone/drone-runner-docker:latest
